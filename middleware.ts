7eeeb06e5280d83db48f43ecfb38ff55ad83361a
anhtuandao1311 <106532352+anhtuandao1311@users.noreply.github.com>
2024-04-03 13:45:07 +0700

    add api/webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,17 @@ import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhooks(.*)",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "profile/:id",
+    "community",
+    "jobs",
+  ],
+  ignoredRoutes: ["/api/webhooks(.*)"],
 })
 
 export const config = {
