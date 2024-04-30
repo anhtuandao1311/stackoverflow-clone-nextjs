cc533f2055f074cda2351fa626083156232488a1
anhtuandao1311 <106532352+anhtuandao1311@users.noreply.github.com>
2024-05-01 02:02:55 +0700

    fix shadcn eslint bugs

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/select"
 import { formUrlQuery } from "@/lib/utils"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { useEffect } from "react"
 
 interface Props {
   filters: {
